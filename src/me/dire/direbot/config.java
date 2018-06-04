4a428cffaf94c675d31912591e8a1c98cede0857
Dire <pday32@gmail.com>
2018-06-04 21:48:08 +0100

    Delete config.java
    sensitive info

--- src/me/dire/direbot/config.java
+++
@@ -1,7 +0,0 @@
-package me.dire.direbot;
-
-public class config {
-
-	public static String token = "NDIzNTEzMTkyMjAzOTQzOTM3.DeDJCQ.dunGPbfOTQP_d2Wvh448L76YAw4";
-	public static String prefix = "./";
-}
