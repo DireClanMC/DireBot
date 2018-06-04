46417a6f97cf50aa2c0b99b2e70682da7007ad4d
Paul Day <pday32@gmail.com>
2018-06-04 21:53:04 +0100

    removed bot token from config

--- src/me/dire/direbot/config.java
+++ src/me/dire/direbot/config.java
@@ -2,6 +2,6 @@ package me.dire.direbot;
 
 public class config {
 
-	public static String token = "NDIzNTEzMTkyMjAzOTQzOTM3.DeDJCQ.dunGPbfOTQP_d2Wvh448L76YAw4";
+	public static String token = "";
 	public static String prefix = "./";
 }
